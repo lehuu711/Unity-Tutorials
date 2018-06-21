ff7dc2c2e7cbc04c280217b839b363871e57617b
lehuu711 <lehuu711@gmail.com>
2018-06-20 23:39:34 -0700

    adding firing shots
    -modified player script to produce shots
    when pressing the fire1 button (default is left ctrl or left mouse-click)
    -set bounds to prevent rapid-firing shots
    
    *still need to despawn shots that have left the field

--- space_shooter/Space_Shooter/Assets/Scripts/PlayerController.cs
+++ space_shooter/Space_Shooter/Assets/Scripts/PlayerController.cs
@@ -13,19 +13,34 @@ public class PlayerController : MonoBehaviour {
 	public float tilt;
 	public Boundary boundary;
 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
 	private Rigidbody rb;
+	private float nextFire;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 	}
 
+	void Update () {
+		//Generate a shot
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate(shot,shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
 	void FixedUpdate () {
+		//Calculate movement
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 		rb.velocity = movement * speed;
 
+		//Make sure ship doesn't fly out of field
 		rb.position = new Vector3 (
 			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
 			0.0f,
