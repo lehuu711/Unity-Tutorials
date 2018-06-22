b0af86f06a6e19b6844962eeaab29a9d55f310dd
lehuu711 <lehuu711@gmail.com>
2018-06-21 20:48:52 -0700

    updated asteroid hazard
    -now adds an explosion when destroyed
    -adds an explosion when the player makes contact
    -added the mover script so the asteroid moves down the screen
    -saved the asteroid as a prefab

--- space_shooter/Space_Shooter/Assets/Scripts/DestroyByContact.cs
+++ space_shooter/Space_Shooter/Assets/Scripts/DestroyByContact.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other) {
-		if (other.tag != "Boundary") {
+		if (other.tag == "Boundary") {
+			return;
+		}
+		if (other.tag == "Player") {
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
+			Instantiate(explosion, transform.position, transform.rotation);
 			Destroy(other.gameObject);
 			Destroy(gameObject);
-		}
 	}
 }
