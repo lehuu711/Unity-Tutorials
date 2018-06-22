0f324972b19bcc9045924e3743403c9e2faebd9a
lehuu711 <lehuu711@gmail.com>
2018-06-21 21:25:04 -0700

    gamecontroller object
    -added a basic gamecontroller object
    -gamecontroller now spawns a single asteroid on startup (to be changed to a series of waves)

---
+++ space_shooter/Space_Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnVals;
+
+	void Start () {
+		SpawnWaves ();
+	}
+
+	void SpawnWaves () {
+		Vector3 spawnPos = new Vector3(Random.Range(-spawnVals.x, spawnVals.x), spawnVals.y, spawnVals.z);
+		Quaternion spawnRot = Quaternion.identity;
+		Instantiate (hazard, spawnPos, spawnRot);
+	}
+}
