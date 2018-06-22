5cc2fcdb492994ce50e2f073bf2854a17a48634a
lehuu711 <lehuu711@gmail.com>
2018-06-21 23:11:00 -0700

    game over and restart
    -added text for game over and restart
    -ends the game when the player dies and prompts a restart message
    -allows a restart via 'R' key
    
    (Note that GUI Text is now deprecated, so one needs to create a Canvas+Text object in place of a GUIText object)

--- space_shooter/Space_Shooter/Assets/Scripts/GameController.cs
+++ space_shooter/Space_Shooter/Assets/Scripts/GameController.cs
@@ -13,13 +13,32 @@ public class GameController : MonoBehaviour {
 	public float startWait;
 	public float waveWait;
 
-	public Text scoreText;
 	public int score;
+	public Text scoreText;
+	public Text restartText;
+	public Text gameOverText;
+
+	private bool gameOver;
+	private bool restart;
 
 	void Start () {
 		score = 0;
 		UpdateScore();
+
+		restart = false;
+		gameOver = false;
+		restartText.text = "";
+		gameOverText.text = "";
+
 		StartCoroutine (SpawnWaves());
+	}
+
+	void Update() {
+		if (restart) {
+			if (Input.GetKeyDown(KeyCode.R)) {
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
 	}
 
 	IEnumerator SpawnWaves () {
@@ -34,6 +53,12 @@ public class GameController : MonoBehaviour {
 				yield return new WaitForSeconds(spawnWait);
 			}
 			yield return new WaitForSeconds(waveWait);
+
+			if (gameOver) {
+				restartText.text = "Press	'R' to restart.";
+				restart = true;
+				break;
+			}
 		}
 	}
 
@@ -44,5 +69,10 @@ public class GameController : MonoBehaviour {
 
 	void UpdateScore () {
 		scoreText.text = "Score " + score;
+	}
+
+	public void GameOver() {
+		gameOverText.text = "Game Over!";
+		gameOver = true;
 	}
 }
