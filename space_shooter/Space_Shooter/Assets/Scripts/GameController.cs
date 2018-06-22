4fc40ed5fce623792d9e0b73316c636a0f4a64cf
lehuu711 <lehuu711@gmail.com>
2018-06-21 21:48:00 -0700

    changed spawn waves
    -changed the SpawnWaves() call to be a coroutine with a yield
    -added new parameters to make the asteroids spawn after each other in waves repeatedly
    -added a script that destroys explosion objects after a set time

--- space_shooter/Space_Shooter/Assets/Scripts/GameController.cs
+++ space_shooter/Space_Shooter/Assets/Scripts/GameController.cs
@@ -6,14 +6,28 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnVals;
+	public int hazardCount;
+
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	void Start () {
-		SpawnWaves ();
+		 StartCoroutine (SpawnWaves());
 	}
 
-	void SpawnWaves () {
-		Vector3 spawnPos = new Vector3(Random.Range(-spawnVals.x, spawnVals.x), spawnVals.y, spawnVals.z);
-		Quaternion spawnRot = Quaternion.identity;
-		Instantiate (hazard, spawnPos, spawnRot);
+	IEnumerator SpawnWaves () {
+		yield return new WaitForSeconds(startWait);
+		while(true) {
+			for (int i=0; i < hazardCount; i++) {
+				Vector3 spawnPos = new Vector3(Random.Range(-spawnVals.x, spawnVals.x),
+																			 spawnVals.y,
+																			 spawnVals.z);
+				Quaternion spawnRot = Quaternion.identity;
+				Instantiate (hazard, spawnPos, spawnRot);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
 	}
 }
