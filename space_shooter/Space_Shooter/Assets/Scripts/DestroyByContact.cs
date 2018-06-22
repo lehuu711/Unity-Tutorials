fb039d02aacb5556f58da6e114f4ed9b76dd8de6
lehuu711 <lehuu711@gmail.com>
2018-06-21 20:38:03 -0700

    added hazard
    -created a new asteroid object
    -added random rotation animation to asteroid
    -allows asteroid to be destroyed by another object (like bullets or player)
    -tagged boundary so it doesn't interfere with asteroid collision

---
+++ space_shooter/Space_Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag != "Boundary") {
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
