207f8832ce1bc43df54f353ad385eb27a2482bce
lehuu711 <lehuu711@gmail.com>
2018-06-22 18:04:10 -0700

    add enemy model
    -added an enemy model to the game
    -added collision and a shot spawn to the enemy model
    -added a new bolt for enemy weaponry

---
+++ space_shooter/Space_Shooter/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	public float delay;
+
+	private float nextFire;
+	private AudioSource boltAudio;
+
+	// Use this for initialization
+	void Start () {
+		boltAudio = GetComponent<AudioSource>();
+		InvokeRepeating ("Fire", delay, fireRate);
+	}
+
+	void Fire () {
+		Instantiate(shot,shotSpawn.position, shotSpawn.rotation);
+		boltAudio.Play();
+	}
+}
