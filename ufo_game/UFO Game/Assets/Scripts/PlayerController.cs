bb93cbece451d92f2739192327d57ea080b475a9
lehuu711 <lehuu711@gmail.com>
2018-06-27 20:52:54 -0700

    bells and whistles
    -added particle systems that add extra effects when winning the game (fireworks, confetti)
    -added smoke trail animation to player
    -added sparkle animations to pick up items
    -added animation folder/files for other pick-up animations (WIP)

--- ufo_game/UFO Game/Assets/Scripts/PlayerController.cs
+++ ufo_game/UFO Game/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
+
+	public ParticleSystem fireworks;
+	public GameObject confettiObject;
 
 	private Rigidbody2D rb;
+	private int count;
 
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
 	}
 
 	void FixedUpdate() {
@@ -17,5 +27,24 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis("Vertical");
 		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 		rb.AddForce(movement * speed);
+		transform.Rotate(new Vector3(0,0,-1));
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag("PickUp")) {
+			Destroy(other.gameObject);
+			count += 1;
+			SetCountText();
+
+			if (count >= 12) {
+				winText.text = "You Win!";
+				fireworks.Play();
+				confettiObject.SetActive(true);
+			}
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString();
 	}
 }
