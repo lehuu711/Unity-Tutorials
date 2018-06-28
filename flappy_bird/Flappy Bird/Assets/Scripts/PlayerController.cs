6a6bc4dcd8d6d2eda2f188e0a53efe5e6d690c6e
lehuu711 <lehuu711@gmail.com>
2018-06-27 22:05:06 -0700

    added sprites to scene, player behavior
    -added the player and background sprites to the scene
    -added colliders to player and ground
    -added script to make player hop when mouseclick
    -different from the tutorial, modify the player velocity only once instead of twice (1 to reset velocity, 2 to add force)
    -prevents player from moving (dead state) once player collides with other object

---
+++ flappy_bird/Flappy Bird/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float upForce;
+
+	private Rigidbody2D rb;
+	private bool isDead;
+
+	void Start() {
+		isDead = false;
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		// Hop if not yet and left-click triggered
+		if (!isDead) {
+			if (Input.GetMouseButtonDown(0)) {
+				// Resets velocity and hops up
+				rb.velocity = new Vector2(0,upForce);
+			}
+		}
+	}
+
+	// Player dies if they collide into anything
+	void OnCollisionEnter2D(Collision2D col) {
+		isDead = true;
+	}
+}
