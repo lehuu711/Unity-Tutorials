391307c9982d604b49c24863a27aa2bb939b7d02
lehuu711 <lehuu711@gmail.com>
2018-06-28 20:47:38 -0700

    column pooling
    -spawns columns by using object pooling (generate all columns offscreen and shift positions as we start the game) and cycle through the array to reposition/recycle the assets
    -added a movemnt script to the columns so it moves to the left

---
+++ flappy_bird/Flappy Bird/Assets/Scripts/ColumnPooler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPooler : MonoBehaviour {
+
+	public int poolSize = 5;
+	public float spawnRate = 4f;
+	public GameObject columnPrefab;
+	public Vector2 columnRange;
+
+	private GameObject[] columns;
+	private Vector2 poolPos;
+	private float timeSinceLastSpawn;
+	private float spawnXPos = 10f;
+	private int columnI = 0;
+
+	// Use this for initialization
+	void Start () {
+		poolPos = new Vector2(-15f, -25f);
+		columns = new GameObject[poolSize];
+		for (int i = 0; i < poolSize; i++) {
+			columns[i] = (GameObject) Instantiate(columnPrefab,
+																						poolPos,
+																						Quaternion.identity);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastSpawn += Time.deltaTime;
+		if (!GameController.instance.gameOver && timeSinceLastSpawn >= spawnRate) {
+			timeSinceLastSpawn = 0;
+			float spawnYPos = Random.Range(columnRange.x,columnRange.y);
+			columns[columnI].transform.position = new Vector2(spawnXPos,spawnYPos);
+			columnI = (columnI + 1) % poolSize;
+		}
+	}
+}
---
+++ flappy_bird/Flappy Bird/Assets/Scripts/ObjectScroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScroller : MonoBehaviour
+{
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Get and store a reference to the Rigidbody2D attached to this GameObject.
+		rb = GetComponent<Rigidbody2D>();
+
+		//Start the object moving.
+		rb.velocity = new Vector2 (GameController.instance.scrollSpeed, 0) *
+									Vector2.left;
+	}
+
+	void Update()
+	{
+		// If the game is over, stop scrolling.
+		if(GameController.instance.gameOver == true)
+		{
+			rb.velocity = Vector2.zero;
+		}
+	}
+}
