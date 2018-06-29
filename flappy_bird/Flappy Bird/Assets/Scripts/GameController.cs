23157017e8db31b3a982374ae5aeebf0df0a2cd6
lehuu711 <lehuu711@gmail.com>
2018-06-28 20:02:04 -0700

    column
    -created a column prefab for a game obstacle
    -adds to score whenever a column obstacle is passed

--- flappy_bird/Flappy Bird/Assets/Scripts/GameController.cs
+++ flappy_bird/Flappy Bird/Assets/Scripts/GameController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
 	public static GameController instance;
 
+	public Text scoreText;
 	public GameObject gameOverText;
 	public float scrollSpeed = 1.5f;
 	public bool gameOver;
+
+	private int score;
 
 	void Awake() {
 		if (instance == null) {
@@ -24,6 +28,11 @@ public class GameController : MonoBehaviour {
 		if (gameOver && Input.GetMouseButtonDown(0)) {
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		}
+	}
+
+	public void AddScore() {
+		score++;
+		scoreText.text = "Score: " + score.ToString();
 	}
 
 	public void GameOver() {
