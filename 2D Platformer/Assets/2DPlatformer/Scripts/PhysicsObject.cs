c425ae11586358c4b0ef9881b4e80e05cb073ca0
lehuu711 <lehuu711@gmail.com>
2018-06-28 22:36:44 -0700

    custom physics
    -added a script that applies developer-set physics (just gravity at the moment)

---
+++ 2D Platformer/Assets/2DPlatformer/Scripts/PhysicsObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour {
+
+	public float gravityMod = 3f;
+
+	protected Vector2 velocity;
+	protected Rigidbody2D rb;
+
+	void OnEnable() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		velocity += gravityMod * Physics2D.gravity * Time.deltaTime;
+
+		Vector2 deltaPos = velocity * Time.deltaTime;
+
+		Vector2 move = Vector2.up * deltaPos.y;
+
+		Movement(move);
+	}
+
+	void Movement(Vector2 move) {
+		rb.position = rb.position + move;
+	}
+}
