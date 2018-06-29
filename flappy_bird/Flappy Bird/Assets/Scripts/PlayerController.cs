6db84ea6c25524d41ce6b1b655cab307deb6b08e
lehuu711 <lehuu711@gmail.com>
2018-06-28 21:17:42 -0700

    position clamping
    -clamps player to the top edge of screen to prevent player from flying offcamera

--- flappy_bird/Flappy Bird/Assets/Scripts/PlayerController.cs
+++ flappy_bird/Flappy Bird/Assets/Scripts/PlayerController.cs
@@ -16,13 +16,20 @@ public class PlayerController : MonoBehaviour {
 		anim = GetComponent<Animator>();
 	}
 
-	// Update is called once per frame
 	void Update() {
 		// Hop if not yet and left-click triggered
 		if (!isDead && Input.GetMouseButtonDown(0)) {
 			// Resets velocity and hops up
 			rb.velocity = new Vector2(0,upForce);
 			anim.SetTrigger("Flap");
+		}
+	}
+
+	void FixedUpdate() {
+		// Clamp the player to the edge of screen and reset movement
+		if (rb.position.y >= Camera.main.orthographicSize - 0.25f) {
+			rb.position = new Vector2(0,Camera.main.orthographicSize - 0.25f);
+			rb.velocity = Vector2.zero;
 		}
 	}
 
